dbdfe720bc3f7f674906cf9e7a47f1dfbf61ee86
fedefredo <federico.fredotovich@gmail.com>
2023-10-25 21:51:39 -0300

    user delete modificado y mejorado

--- js/users.js
+++ js/users.js
@@ -114,6 +114,109 @@ const usersArray = [
 //Obtener el body de la tabla 
 const tableBody = document.getElementById("table-body")
 const searchInput = document.querySelector("#search")
+const userForm = document.querySelector("form#user-form")
+const submitBtn = userForm.querySelector('button[type=submit]')
+
+userForm.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+    const el = evt.target.elements;
+
+    // Deberia cortar la ejecucion de la funcion callback del eveneto submit
+    // Password y password2 sean distintos
+    if (el.password.value !== el.password2.value) {
+        alert("Las contraseñas no coinciden")
+        return
+    }
+
+        // Email ya exista
+        const emailExist = usersArray.find((user) => {
+            if (user.email === el.email.value) {
+                return true
+            }
+        })
+
+        if(emailExist && el.id.value !== emailExist.id) {
+            Swal.fire({
+                title: 'El correo ya existe',
+                icon: 'error'
+            })
+            return
+        }
+
+        // if else convencional
+        // let id; 
+        // if (el.id.value) {
+        //     id = el.id.value
+        // } else {
+        //     id = crypto.randomUUID() //el id no puede repetirse
+        // }
+
+    //Operador ternario => condicion ? condicionTrue : condicionFalse
+    const id = el.id.value ? el.id.value : crypto.randomUUID()
+
+
+    const user = {
+        fullname: el.fullname.value,
+        age: el.age.valueAsNumber,
+        email: el.email.value,
+        password: el.password.value,
+        active: el.active.checked, //usar .checked cuando uso active
+        bornDate: new Date(el.bornDate.value).getTime(),
+        location: el.location.value,
+        id: id, 
+        image: el.image.value
+    }
+
+    //Tenemos dos posibles acciones a realizar
+    //Opcion a) Al estar editando deberia reemplazar el usuario a editar con su info actualizada 
+    //Opcion b) Agregue un usuario nuevo
+    
+    //Pregunto si tengo id para saber si estoy editando o no 
+    if (el.id.value) {
+        //editando
+        const indice = usersArray.findIndex(usuario => {
+            if (usuario.id === el.id.value) {
+                return true
+            }
+        })
+
+        //reemplazo el usuario con los datos nuevos del formulario
+        usersArray[indice] = user
+        //Swal.fire('Usuario editado', 'Los datos del usuario se actualizaron correctamente', 'success')
+        Swal.fire({
+            title: 'Usuario editado',
+            text: 'Los datos del usuario se actualizaron correctamente',
+            icon: 'success',
+            timer: 1000
+        })
+    } else {
+        //Agregando un usuario nuevo
+        usersArray.push(user)
+        //Swal.fire('Usuario agregado', 'Usuario se creo correctamente', 'success')
+        Swal.fire({
+            title: 'Usuario agregado',
+            text: 'Usuario se creo correctamente',
+            icon: 'success',
+            timer: 1000
+        })
+    }
+
+    pintarUsuarios(usersArray)
+
+    resetearFormulario()
+})
+
+function resetearFormulario() {
+    //Reseteo formulario
+    userForm.reset()
+    //Activo los inputs, si estaban desactivados
+    userForm.elements.password.disabled = false
+    userForm.elements.password2.disabled = false
+    //Remuevo la clase editar
+    submitBtn.classList.remove('btn-edit')
+    //Vuelvo el texto del boton a su valor por defecto
+    submitBtn.innerHTML = 'Agregar Usuario'
+}
 
 //Escuchar cuando el usuario presiona una tecla en el input search
 searchInput.addEventListener("keyup", (eventito) => {
@@ -155,9 +258,11 @@ function pintarUsuarios(arrayPintar) {
             <td class="user-age">${user.age}</td>
             <td class="user-date">${formatDate(user.bornDate)}</td>
             <td>
-                <button class="action-btn btn-danger" title="Borrar usuario" onclick="borrarUsuario(${indice})">
+                <button class="action-btn btn-danger" title="Borrar usuario" onclick="borrarUsuario('${user.id}')">
                 <i class="fa-solid fa-trash-can"></i>
                 </button>
+
+                <button class="action-btn" title="Editar Usuario" onclick="editarUsuario('${user.id}')"><i class="fa-solid fa-pen-to-square"></i></button>
             </td>
         </tr>`
     })
@@ -165,10 +270,56 @@ function pintarUsuarios(arrayPintar) {
 
 pintarUsuarios(usersArray)
 
-function borrarUsuario(indice) {
-    usersArray.splice(indice, 1)
+function borrarUsuario(ID, nombre) {
+    const confirmDelete = confirm(`Realmente desea borrar este usuario ${nombre}`)
 
-    pintarUsuarios(usersArray)
+    if (confirmDelete) {
+        const indice = usersArray.findIndex(user => user.id === ID)
+        usersArray.splice(indice, 1)
+        pintarUsuarios(usersArray)
+    }
+}
+
+function editarUsuario(idBuscar) {
+    //Buscar un usuario con id 
+    const userEdit = usersArray.find((usuario) => {
+        //deberia devolver un true, segun la condicion id que me enviaron === al usuario que estoy iterando
+        if(usuario.id === idBuscar) {
+            return true
+        }
+    })
+    //Indicar que el usuario no fue encontrado
+    if (userEdit === undefined) {
+       Swal.fire('Error al editar', 'No se pudo editar el usuario', 'error')
+        return
+    }
+    console.log(userEdit)
+
+    //Rellenar el formulario con los datos del usuario a ingresar
+    const el = userForm.elements
+
+    el.id.value = userEdit.id
+    el.age.value = userEdit.age
+    el.fullname.value = userEdit.fullname
+    el.email.value = userEdit.email
+    el.image.value = userEdit.image
+    el.location.value = userEdit.location
+    el.active.checked = userEdit.active
+    el.password.value = userEdit.password
+    el.password.disabled = true
+    el.password2.value = userEdit.password2
+    el.password2.disabled = true
+    el.bornDate.value = formatInputDate(userEdit.bornDate)
+
+    console.log(formatInputDate(userEdit.bornDate))
+
+    //Cambiar el nombre del boton a editar usuario
+    submitBtn.classList.add('btn-edit')
+    submitBtn.innerHTML = 'Editar Usuario'
+    //Deshabilitar los input de contraseña
+
+    
+    
 }
 
 
@@ -185,6 +336,18 @@ function formatDate(fecha) {
 
     return fechaFormateada
 
+}
+
+function formatInputDate(fechaInput) {
+    const fecha = new Date(fechaInput)
+    const year = fecha.getFullYear()
+    const month = fecha.getMonth() + 1
+    if (month < 10){
+        return 0 + month
+    }
+    const day = fecha.getDate()
+    const formatedDate = `${year}-${month}-${day}`
+    return formatedDate
 }
 
 
